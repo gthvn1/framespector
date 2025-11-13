3722b84cd5e7d8301c9c156c27d50bd6e5639b15
Guillaume <guillaume.thouvenin@vates.tech>
2025-11-13 14:54:40 +0100

    Use struct to allow closing socket in in Drop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
 use std::os::unix::io::RawFd;
 
+pub struct Socket {
+    fd: RawFd,
+}
+
+impl Socket {
+    pub fn new() -> std::io::Result<Self> {
+        let fd = unsafe { libc::socket(libc::AF_PACKET, libc::SOCK_RAW, 0) };
+        if fd < 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+        Ok(Socket { fd })
+    }
+
+    pub fn fd(&self) -> RawFd {
+        self.fd
+    }
+}
+
+impl Drop for Socket {
+    fn drop(&mut self) {
+        unsafe {
+            libc::close(self.fd);
+        }
+    }
+}
+
 fn main() {
-    let _sockfd = match create_socket() {
+    let _sockfd = match Socket::new() {
         Ok(sockfd) => sockfd,
         Err(e) => {
             println!("{e}");
@@ -10,17 +36,4 @@ fn main() {
     };
 
     println!("Socket created");
-}
-
-fn create_socket() -> Result<i32, String> {
-    // https://docs.rs/libc/latest/libc/
-    // AF_PACKET -> Low-level packet interface (man 7 packet)
-    //           == communication domain; this is the protocol family
-    // SOCK_RAW -> raw network protocol access
-    let sockfd: RawFd = unsafe { libc::socket(libc::AF_PACKET, libc::SOCK_RAW, 0) };
-    if sockfd == -1 {
-        return Err(String::from("Failed to create the socket"));
-    }
-
-    Ok(sockfd)
 }
