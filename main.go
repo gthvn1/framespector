4403e5b09234d5cd8c5c73e2339d94e28fbc7cda
Guillaume <guillaume.thouvenin@vates.tech>
2025-11-14 18:04:53 +0100

    Introduce new network module

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"example.com/framespector/network"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, Sailor")
+	network.SetupNetwork()
+}
---
+++ network/setup.go
@@ -0,0 +1,7 @@
+package network
+
+import "fmt"
+
+func SetupNetwork() {
+	fmt.Println("TODO: Setup the network")
+}
